ec2cd5c0630b20194653596d26bdc671c4f9937e
Luis Andres Cardozo <luisandrescardozo@gmail.com>
2018-04-08 22:22:01 -0500

    Graphic

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Lands
+{ 
+    using Xamarin.Forms; //Recomendacion Meter los Usign por dentro de los namespace (puedenir por dentro o por fuera)por que es mas facil copiar codigo entre clases -> por la ruta
+    using Lands.Views;
+
+	public partial class App : Application
+	{
+        #region CONSTRUCTOR
+        public App() //contructor
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage( new LoginPage());
+        }
+        #endregion
+
+        #region METODOS
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
---
+++ lands/Lands/Lands/InfraStructure/InstanceLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.InfraStructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+
+        #region Properties
+
+        public MainViewModel Main
+        {
+
+            get; set;
+        }
+
+
+        #endregion
+
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+
+    }
+}
