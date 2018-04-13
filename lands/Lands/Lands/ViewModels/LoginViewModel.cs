20447fc911817a7b3b539ccf80aec51514bb6a0b
Luis Andres Cardozo <luisandrescardozo@gmail.com>
2018-04-12 23:32:41 -0500

    LoginViewModel

--- lands/Lands/Lands/ViewModels/LoginViewModel.cs
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Lands.ViewModels
 {
+
+    //Recomendacion Using por dentro del namespaces 
+
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows.Input;
+
+
     public class LoginViewModel
     {
+
+        #region Properties
+
+        public string Email { get; set; }
+
+        public string  Password { get; set; }
+
+        public bool IsRunning { get; set; }
+
+        public bool IsRemembered { get; set; }
+
+        #endregion
+
+        #region Constructs
+
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+
+        #region Commands
+
+        public ICommand LoginCommand { get; set; }
+
+
+        #endregion
+
     }
 }
