90316c54a78de77ba0471a21bc02d73cc3aa2b4b
Luis Andres Cardozo <luisandrescardozo@gmail.com>
2018-04-24 23:20:04 -0500

    13 Show Lands in a ListView

--- lands/Lands/Lands/ViewModels/LandsViewModel.cs
+++ lands/Lands/Lands/ViewModels/LandsViewModel.cs
@@ -9,6 +9,10 @@ namespace Lands.ViewModels
     using Models;
     using Xamarin.Forms;
     using System.Collections.Generic;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Linq;
 
     public class LandsViewModel : BaseViewModel //Hereda de BaseViewModel Para poder refrescar en Pantalla 
     {
@@ -22,6 +26,9 @@ namespace Lands.ViewModels
 
         #region Atributtes
         private ObservableCollection<Land> lands; //Creamos una lista tipo ObservableCollection para pintar en un ListView de la clase Land
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> landList;
         #endregion
 
         #region Properties
@@ -35,12 +42,35 @@ namespace Lands.ViewModels
                 SetValue(ref this.lands, value);
 
             }
-            #endregion
-
-
-
-
         }
+
+        public bool IsRefreshing 
+        {
+            //Para refrescar las propiedaeds en tiempo de ejecucion mediante la clase BaseViewModel
+            get { return this.isRefreshing; }
+            set
+            {
+                SetValue(ref this.isRefreshing, value);
+
+            }
+        }
+
+        public string Filter
+        {
+            //Para refrescar las propiedaeds en tiempo de ejecucion mediante la clase BaseViewModel
+            get { return this.filter; }
+            set
+            {
+                SetValue(ref this.filter, value);
+                this.Search();
+
+            }
+        }
+
+
+
+        #endregion
+
 
         #region Contructor
         public LandsViewModel()
@@ -55,11 +85,12 @@ namespace Lands.ViewModels
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
             var connection = await this.apiService.CheckConnection();
 
             if(!connection.IsSuccess)
             {
-
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     connection.Message,
@@ -67,10 +98,11 @@ namespace Lands.ViewModels
                     );
 
                 await Application.Current.MainPage.Navigation.PopAsync(); //Desapila, se devuelve un atras en codigo
-
+          
                 return;
 
             }
+
 
             var response = await this.apiService.GetList<Land> (
                 "http://restcountries.eu",
@@ -79,6 +111,7 @@ namespace Lands.ViewModels
 
             if(!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     response.Message,
@@ -89,8 +122,57 @@ namespace Lands.ViewModels
 
             }
 
-            var list = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
+            this.landList = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landList);
+            this.IsRefreshing = false;
+        }
+
+        #endregion
+
+
+        #region Commands
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+
+                return new RelayCommand(LoadLands);
+
+            }
+            
+        }
+
+        public ICommand SearchCommand
+        {
+            get
+            {
+
+                return new RelayCommand(Search);
+
+            }
+
+        }
+
+        private void Search()
+        {
+            
+            if(string.IsNullOrEmpty(this.Filter))
+            {
+
+                this.Lands = new ObservableCollection<Land>(this.landList);
+            }
+            else
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landList.Where
+                    (l => l.Name.ToLower().Contains(this.Filter.ToLower()) ||
+                          l.Capital.ToLower().Contains(this.Filter.ToLower())
+                    ));
+
+
+            }
+
         }
 
         #endregion
