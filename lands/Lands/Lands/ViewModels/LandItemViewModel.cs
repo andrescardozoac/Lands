1215e8d4058b20e190fb2953e4fd1ceeb6be9c72
Luis Andres Cardozo <luisandrescardozo@gmail.com>
2018-05-07 23:05:48 -0500

    15 Show LandsDetail

--- lands/Lands/Lands/ViewModels/LandItemViewModel.cs
+++ lands/Lands/Lands/ViewModels/LandItemViewModel.cs
@@ -1,10 +1,40 @@
 ﻿
 namespace Lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Lands.Views;
     using Models;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class LandItemViewModel : Land
     {
+        #region Command
+        public ICommand SelectLandCommand
+        {
+
+            get
+            {
+
+                return new RelayCommand(SelectLand);
+
+            }
+
+
+        }
+
+        private async void SelectLand()
+        {
+
+            MainViewModel.GetInstance().Land = new LandViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+        }
+
+
+        #endregion
+
+
 
 
     }
--- lands/Lands/Lands/ViewModels/LandViewModel.cs
+++ lands/Lands/Lands/ViewModels/LandViewModel.cs
@@ -1,7 +1,34 @@
 ﻿
+
 namespace Lands.ViewModels
 {
+
+    using Lands.Models;
+
+
     public class LandViewModel
     {
+
+        #region Properties
+
+        public Land Land
+            {
+
+            get;
+            set;
+
+
+            }
+
+
+        #endregion
+
+
+        private LandItemViewModel landItemViewModel;
+
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
     }
 }
